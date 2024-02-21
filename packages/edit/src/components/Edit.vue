3a5854528e1e74cc95b19f52ad400740d71abd6b
underscope <underscope@gmail.com>
2024-02-21 08:20:57 +0100

    Handle build issue

--- packages/edit/src/components/Edit.vue
+++ packages/edit/src/components/Edit.vue
@@ -49,7 +49,7 @@ const emit = defineEmits(['save']);
 const editor = useEditor({
   content: props.element.data.content,
   extensions: [StarterKit],
-});
+}) as any;
 
 watch(
   () => props.isFocused,
