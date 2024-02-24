58123b67d770e08fa1e83e4d33e57bda4f7f03b2
rkusan00 <renato.kusanovic@gmail.com>
2024-02-25 00:05:04 +0100

    Update structure

--- packages/edit/src/components/EditorToolbar.vue
+++ packages/edit/src/components/EditorToolbar.vue
@@ -1,7 +1,7 @@
 <template>
   <VBtnGroup density="compact" variant="text">
-    <template v-for="(group, index) in actions" :key="index">
-      <VTooltip v-for="btn in group" :key="btn.label" location="bottom">
+    <template v-for="(btn, index) in toolbarItems">
+      <VTooltip v-if="'action' in btn" :key="btn.label" location="bottom">
         <template #activator="{ props }">
           <VBtn
             :active="'isActive' in btn && editor.isActive(btn.isActive)"
@@ -18,7 +18,12 @@
         </template>
         {{ btn.label }}
       </VTooltip>
-      <VDivider class="mx-1" vertical />
+      <component
+        :is="btn.component"
+        v-else-if="'component' in btn"
+        :key="btn.component"
+      />
+      <VDivider v-else :key="index" class="mx-1" vertical />
     </template>
   </VBtnGroup>
 </template>
@@ -33,113 +38,109 @@ interface Action {
   icon: string;
 }
 
-const actions: Action[][] = [
-  [
-    {
-      label: 'Undo',
-      action: ['undo'],
-      icon: 'undo',
-    },
-    {
-      label: 'Redo',
-      action: ['redo'],
-      icon: 'redo',
-    },
-  ],
-  [
-    {
-      label: 'Bold',
-      isActive: 'bold',
-      action: ['toggleBold'],
-      icon: 'format-bold',
-    },
-    {
-      label: 'Italic',
-      isActive: 'italic',
-      action: ['toggleItalic'],
-      icon: 'format-italic',
-    },
-    {
-      label: 'Underline',
-      isActive: 'underline',
-      action: ['toggleUnderline'],
-      icon: 'format-underline',
-    },
-    {
-      label: 'Strikethrough',
-      isActive: 'strike',
-      action: ['toggleStrike'],
-      icon: 'format-strikethrough',
-    },
-  ],
-  [
-    {
-      label: 'Horizontal line',
-      action: ['setHorizontalRule'],
-      icon: 'minus',
-    },
-  ],
-  [
-    {
-      label: 'Numbered list',
-      isActive: 'orderedList',
-      action: ['toggleOrderedList'],
-      icon: 'format-list-numbered',
-    },
-    {
-      label: 'Numbered list',
-      isActive: 'bulletList',
-      action: ['toggleBulletList'],
-      icon: 'format-list-bulleted',
-    },
-    {
-      label: 'Decrease indent',
-      isActive: 'bulletList',
-      action: ['liftListItem', 'listItem'],
-      icon: 'format-indent-decrease',
-    },
-    {
-      label: 'Increase indent',
-      isActive: 'bulletList',
-      action: ['sinkListItem', 'listItem'],
-      icon: 'format-indent-increase',
-    },
-  ],
-  [
-    {
-      label: 'Superscript',
-      isActive: 'superscript',
-      action: ['toggleSuperscript'],
-      icon: 'format-superscript',
-    },
-    {
-      label: 'Strikethrough',
-      isActive: 'subscript',
-      action: ['toggleSubscript'],
-      icon: 'format-subscript',
-    },
-  ],
-  [
-    {
-      label: 'Quote',
-      isActive: 'blockquote',
-      action: ['toggleBlockquote'],
-      icon: 'format-quote-close',
-    },
-    {
-      label: 'Code',
-      isActive: 'code',
-      action: ['toggleCode'],
-      icon: 'code-tags',
-    },
-  ],
-  [
-    {
-      label: 'Clear formatting',
-      action: ['unsetAllMarks'],
-      icon: 'format-clear',
-    },
-  ],
+interface Component {
+  component: string;
+}
+
+const toolbarItems: (Action | Component | { type: string })[] = [
+  {
+    label: 'Undo',
+    action: ['undo'],
+    icon: 'undo',
+  },
+  {
+    label: 'Redo',
+    action: ['redo'],
+    icon: 'redo',
+  },
+  { type: 'divider' },
+  {
+    label: 'Bold',
+    isActive: 'bold',
+    action: ['toggleBold'],
+    icon: 'format-bold',
+  },
+  {
+    label: 'Italic',
+    isActive: 'italic',
+    action: ['toggleItalic'],
+    icon: 'format-italic',
+  },
+  {
+    label: 'Underline',
+    isActive: 'underline',
+    action: ['toggleUnderline'],
+    icon: 'format-underline',
+  },
+  {
+    label: 'Strikethrough',
+    isActive: 'strike',
+    action: ['toggleStrike'],
+    icon: 'format-strikethrough',
+  },
+  { type: 'divider' },
+  {
+    label: 'Horizontal line',
+    action: ['setHorizontalRule'],
+    icon: 'minus',
+  },
+  { type: 'divider' },
+  {
+    label: 'Numbered list',
+    isActive: 'orderedList',
+    action: ['toggleOrderedList'],
+    icon: 'format-list-numbered',
+  },
+  {
+    label: 'Numbered list',
+    isActive: 'bulletList',
+    action: ['toggleBulletList'],
+    icon: 'format-list-bulleted',
+  },
+  {
+    label: 'Decrease indent',
+    isActive: 'bulletList',
+    action: ['liftListItem', 'listItem'],
+    icon: 'format-indent-decrease',
+  },
+  {
+    label: 'Increase indent',
+    isActive: 'bulletList',
+    action: ['sinkListItem', 'listItem'],
+    icon: 'format-indent-increase',
+  },
+  { type: 'divider' },
+  {
+    label: 'Superscript',
+    isActive: 'superscript',
+    action: ['toggleSuperscript'],
+    icon: 'format-superscript',
+  },
+  {
+    label: 'Subscript',
+    isActive: 'subscript',
+    action: ['toggleSubscript'],
+    icon: 'format-subscript',
+  },
+  { type: 'divider' },
+  {
+    label: 'Quote',
+    isActive: 'blockquote',
+    action: ['toggleBlockquote'],
+    icon: 'format-quote-close',
+  },
+  {
+    label: 'Code',
+    isActive: 'code',
+    action: ['toggleCode'],
+    icon: 'code-tags',
+  },
+  { type: 'divider' },
+  {
+    label: 'Clear formatting',
+    action: ['unsetAllMarks'],
+    icon: 'format-clear',
+  },
 ];
 </script>
 
