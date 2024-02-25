c504e91b4639a58baa0ae62b7f0810dc882688a3
rkusan00 <renato.kusanovic@gmail.com>
2024-02-25 16:33:34 +0100

    Add highlight

--- packages/edit/src/components/extensions/index.ts
+++ packages/edit/src/components/extensions/index.ts
@@ -1,14 +1,18 @@
+import Color from '@tiptap/extension-color';
+import Highlight from '@tiptap/extension-highlight';
 import Placeholder from '@tiptap/extension-placeholder';
 import StarterKit from '@tiptap/starter-kit';
 import Subscript from '@tiptap/extension-subscript';
 import Superscript from '@tiptap/extension-superscript';
 import TextAlign from '@tiptap/extension-text-align';
 import TextStyle from '@tiptap/extension-text-style';
-import Color from '@tiptap/extension-color';
 import Underline from '@tiptap/extension-underline';
 
 export default [
   Color,
+  Highlight.configure({
+    multicolor: true,
+  }),
   StarterKit,
   Subscript,
   Superscript,
