09139267e3844b0ec63a106bfdb726d18d1671d8
rkusan00 <renato.kusanovic@gmail.com>
2024-07-08 16:45:59 +0200

    Remove side toolbar

--- packages/edit/src/index.ts
+++ packages/edit/src/index.ts
@@ -2,13 +2,11 @@ import baseManifest from '@tailor-cms/ce-html-default-manifest';
 import type { ElementManifest } from '@tailor-cms/ce-html-default-manifest';
 
 import Edit from './components/Edit.vue';
-import SideToolbar from './components/SideToolbar.vue';
 import TopToolbar from './components/TopToolbar.vue';
 
 const manifest: ElementManifest = {
   ...baseManifest,
   Edit,
-  SideToolbar,
   TopToolbar,
 };
 
